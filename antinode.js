92b8acaa321b45061261a8ceba1866cc90c5a9d2
SegunNano <segunfadipe97@gmail.com>
2023-07-29 21:49:18 +0100

    updated

--- antinode.js
+++ antinode.js
@@ -10,26 +10,44 @@ let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
+
 p1Button.addEventListener('click', function() {
     if(!isGameOver) {
-        p1Score += 1;
+        addPlus = addition();
+        p1Score += addPlus;
         if (p1Score === winningScore) {
-            isGameOver = true
+            isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p2Button.disabled = true;
+            p1Button.disabled = true;
+        } else {
+            p2Button.disabled = false;
+            p1Button.disabled = true
         }
         p1Display.textContent = p1Score;
+        ;
     };
 });
 
-
 p2Button.addEventListener('click', function() {
     if(!isGameOver) {
-        p2Score += 1;
+        addPlus = addition();
+        p2Score += addPlus;
         if (p2Score === winningScore) {
-            isGameOver = true
+            isGameOver = true;
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+        } else {
+            p1Button.disabled = false;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     };
 });
+
 
 winningScoreSelect.addEventListener('change', function() {
     winningScore = parseInt(this.value);
@@ -39,9 +57,23 @@ winningScoreSelect.addEventListener('change', function() {
 resetButton.addEventListener('click', reset);
 
 function reset() {
-        isGameOver = false; 
-        p1Score = 0;
-        p2Score = 0;
-        p1Display.textContent = 0;
-        p2Display.textContent = 0;
+    isGameOver = false; 
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-danger', 'has-text-success');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+}
+function addition() {
+    random = Math.random();
+    if (random < 0.5) {
+        increment = 0
+    } else {
+        increment = 1
+    }
+    console.log(increment);
+    return increment
 }
